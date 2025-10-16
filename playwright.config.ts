d3fbf3c40ae147ec8d3ffff5576ae174b4c4faf1
Aldrin Batac <srbatc12@gmail.com>
2025-10-16 08:47:40 +0800

    Set workers to undefined to automatically detect the most efficient number of workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 3 : 3,
-  workers: process.env.CI ? 1 : undefined,
+  workers: undefined,
   reporter: 'html',
   expect: { timeout: 60000 }, // 60 seconds to expect
   timeout: 300000, // 5 minutes per test
